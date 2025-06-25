1762b0d931acee7f17b01eec66891f81d6b75a8f
Niranjan <156616768+Niranjang07@users.noreply.github.com>
2025-06-24 21:14:03 -0500

    Update Experience.jsx

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -11,8 +11,8 @@ function Experience() {
 
         <div className="experience-item">
           <h3><FaBriefcase /> Data Engineer</h3>
-          <p><FaBuilding /> <strong>Data Pro IT (Marketing Product & OPS)</strong>, Hyderabad, India</p>
-          <p><FaClock /> August 2021 – February 2022</p>
+          <p><FaBuilding /> <strong>Datapro Information  Technology Pvt Ltd (Marketing Product & OPS)</strong>, Hyderabad, India</p>
+          <p><FaClock /> May 2021 – February 2022</p>
           <ul>
             <li><FaTasks /> Improved dashboard performance by 30% by optimizing and maintaining analytics dashboards.</li>
             <li><FaTasks /> Accelerated stakeholder data access by 25% through automation and advanced visualizations using SQL, Python, and Tableau.</li>
@@ -24,19 +24,20 @@ function Experience() {
         <div className="experience-item">
           <h3><FaBriefcase /> Data Engineer</h3>
           <p><FaBuilding /> <strong>Tech Mahindra (Pipelines & Enablement)</strong>, Hyderabad, India</p>
-          <p><FaClock /> March 2022 – April 2023</p>
+          <p><FaClock /> April 2022 – August 2023</p>
           <ul>
             <li><FaTasks /> Designed and developed automated ETL pipelines using Apache Airflow and ETL tools to centralize telecom data.</li>
             <li><FaTasks /> Built and optimized Spark/Python workflows for real-time processing of call detail records and usage metrics.</li>
             <li><FaTasks /> Engineered SQL scripts (DDL/DML) for schema evolution and OLTP-OLAP data synchronization.</li>
-            <li><FaTasks /> Implemented data validation, monitoring, and quality assurance processes, resolving pipeline issues proactively.</li>
-            <li><FaTasks /> Collaborated with domain experts to deploy scalable data solutions supporting predictive analytics and churn modeling.</li>
+            <li><FaTasks /> Created custom Airflow DAGs with parameterized configurations to manage data refresh frequency, increasing pipeline flexibility and
+maintainability across multiple environments.</li>
+            <li><FaTasks /> Collaborated with telecom SMEs to support use cases like churn prediction, usage trends, and performance reporting.</li>
           </ul>
         </div>
 
         <div className="experience-item">
           <h3><FaBriefcase /> Software Engineer</h3>
-          <p><FaBuilding /> <strong>Manas Solutions Pvt Ltd</strong>, Hyderabad, India (Remote)</p>
+          <p><FaBuilding /> <strong>Tech Mahindra</strong>, Hyderabad, India (Remote)</p>
           <p><FaClock /> May 2024 – September 2024</p>
           <ul>
             <li><FaTasks /> Improved front-end performance by 20% by building and optimizing web apps using React.js and JavaScript.</li>
